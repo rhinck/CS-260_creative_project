f6b55b90d6cad9fe1662c2712dd11fadabaefdab
Robert Hinckley <37898062+rhinck@users.noreply.github.com>
2020-09-25 16:02:49 -0600

    New Feature: Added about page (RD)

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -1,12 +1,13 @@
 <template>
-  <b-navbar class="absolute" toggleable="lg">
+  <b-navbar toggleable="lg">
     <b-navbar-brand href="#">Company Name</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="m-3">
-        <b-nav-item href="#">About</b-nav-item>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
         <b-nav-item href="#">Contact Us</b-nav-item>
         <b-nav-item href="#">Product Page</b-nav-item>
       </b-navbar-nav>
@@ -22,10 +23,5 @@
 .navbar-nav,
 .nav-link {
   font-size: 1.2rem;
-}
-
-.absolute {
-  position: absolute;
-  width: 100%;
 }
 </style>
---
+++ src/views/About/About.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-container>
+    <h1 class="text-center">About</h1>
+
+    <b-row class="row mt-5">
+      <b-col cols="12" md="" class="d-flex justify-content-center px-4">
+        <b-img
+          src="@/assets/images/pumped-man.jpg"
+          fluid
+          class="description__image"
+        />
+      </b-col>
+      <b-col
+        cols="12"
+        md="6"
+        class="m-1 px-4 d-flex flex-column justify-content-center align-items-center"
+      >
+        <h3 class="my-3">Subheader</h3>
+        <p class="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse velit
+          placeat magnam natus animi earum dolores aliquid quas molestias
+          soluta? Qui illo atque alias odit cupiditate omnis, iste quo
+          similique?
+        </p>
+      </b-col>
+    </b-row>
+
+    <b-row class="row mt-5">
+      <b-col
+        cols="12"
+        md="6"
+        class="order-1 order-md-0 m-1 d-flex flex-column justify-content-center align-items-center px-4"
+      >
+        <h3 class="my-3">Subheader</h3>
+        <p class="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse velit
+          placeat magnam natus animi earum dolores aliquid quas molestias
+          soluta? Qui illo atque alias odit cupiditate omnis, iste quo
+          similique?
+        </p>
+      </b-col>
+      <b-col
+        cols="12"
+        md=""
+        class="order-0 order-md-1 d-flex justify-content-center px-4"
+      >
+        <b-img
+          src="@/assets/images/pumped-man.jpg"
+          fluid
+          class="description__image"
+        />
+      </b-col>
+    </b-row>
+
+    <b-row class="row my-5">
+      <b-col cols="12" md="" class="d-flex justify-content-center px-4">
+        <b-img
+          src="@/assets/images/pumped-man.jpg"
+          fluid
+          class="description__image"
+        />
+      </b-col>
+      <b-col
+        cols="12"
+        md="6"
+        class="m-1 d-flex flex-column justify-content-center align-items-center px-4"
+      >
+        <h3 class="my-3">Subheader</h3>
+        <p class="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse velit
+          placeat magnam natus animi earum dolores aliquid quas molestias
+          soluta? Qui illo atque alias odit cupiditate omnis, iste quo
+          similique?
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
