ee0f0e61bb798ed25229d616c85f03d911e72da4
Robert Hinckley <37898062+rhinck@users.noreply.github.com>
2020-09-25 15:29:05 -0600

    New feature: Working basic home page

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,41 @@
 <template>
   <div>
-    <b-jumbotron
-      fluid
-      class="bg-cover d-flex justify-content-center align-middle"
-    >
-      <b-form-input
-        id="input-1"
-        type="email"
-        required
-        placeholder="Enter email"
-      ></b-form-input>
+    <b-jumbotron fluid class="bg-cover d-flex align-items-center">
+      <div class="d-flex justify-content-center">
+        <b-form-input
+          id="input-1"
+          type="email"
+          required
+          placeholder="Enter email"
+          class="w-50"
+        ></b-form-input>
+        <b-button class="ml-2">
+          Submit
+        </b-button>
+      </div>
     </b-jumbotron>
+    <b-container>
+      <b-row class="row">
+        <b-col cols="12" md="" class="d-flex justify-content-center">
+          <b-img
+            src="@/assets/images/pumped-man.jpg"
+            fluid
+            class="description__image"
+          />
+        </b-col>
+        <b-col cols="12" md="6" class="m-1">
+          <h3 class="my-3">Subheader</h3>
+          <p class="my-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse velit
+            placeat magnam natus animi earum dolores aliquid quas molestias
+            soluta? Qui illo atque alias odit cupiditate omnis, iste quo
+            similique?
+          </p>
+
+          <b-button>Learn more</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -26,5 +51,13 @@ export default {};
   background-position-y: 25%;
   background-repeat: no-repeat;
   height: 50vh;
+}
+
+.description__image {
+  width: 85%;
+}
+
+.row {
+  margin-top: 100px;
 }
 </style>
